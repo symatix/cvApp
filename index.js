bb353bb8055d0ac30742c6227bb621150d12daf9
Dino Kraljeta <pockshocks@gmail.com>
2017-09-19 19:16:30 +0200

    branching feature

--- index.js
+++ index.js
@@ -24,4 +24,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-console.log("/*** SERVER STARTED ***/")
+console.log("/*** and aaaaaAAWWAAAaaayyyyy we GOOOOoooooOOO ***/")
